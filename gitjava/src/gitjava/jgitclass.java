9ba0993cec5408e73b066c5c8b4bb6bdfc254e5e
venkatmanikanta516 <venkat.purushothapu@gmail.com>
2020-09-15 12:35:08 +0530

    java Git

---
+++ gitjava/src/gitjava/jgitclass.java
@@ -0,0 +1,16 @@
+package gitjava;
+
+public class jgitclass {
+	
+	  public static void main(String[] args) {
+	        
+	        int first = 10;
+	        int second = 20;
+
+	        System.out.println("Enter two numbers: " + first + " " + second);
+	        int sum = first + second;
+
+	        System.out.println("The sum is: " + sum);
+	    }
+
+}
